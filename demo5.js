54f0077ba7155d816192110187f795750b82eb75
Masood Ahmed56 <115347725+masaoodahmedsarki@users.noreply.github.com>
2024-03-01 18:05:11 +0500

    data types

---
+++ demo5.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as newer version
+
+//alert(4+4) // we are using nodejs not browser
+
+console.log(4 + 4)
+ console.log("Noreen")
+
+ let name = "Masood"
+ let age = 19
+ let c = false
+ let state;
+
+ // Number => 2 to greater 53
+ //bigint
+ // string => ""
+ //boolean true/false
+ // null standalone value
+ //undefined =>
+ // symbol => unique
+ 
+ // object
+
+ console.log(typeof undefined); //undefined
+ console.log(typeof null); // object 
